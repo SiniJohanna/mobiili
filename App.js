8fc3289c811724dcec782eb2158daf6a7249a692
SiniJohanna <82939474+SiniJohanna@users.noreply.github.com>
2021-10-26 18:01:45 +0300

    Add files via upload

---
+++ App.js
@@ -0,0 +1,23 @@
+import { StatusBar } from 'expo-status-bar';
+import React from 'react';
+import { StyleSheet, Text, View } from 'react-native';
+
+export default function App() {
+  return (
+    <View style={styles.container}>
+      <Text>Terve!</Text>
+      <Text>Tästä alkaa mobiiliohjelmointi.</Text>
+      <Text>Have fun with it!</Text>
+      <StatusBar style="auto" />
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: 'pink',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
